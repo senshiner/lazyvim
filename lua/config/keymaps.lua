c128f192f8f92f6c8393c2f8622e294ebcbbd649
Sendi Winoto <sendiw.cep@gmail.com>
2026-07-15 17:25:13 +0700

    revert: remove VS Code-like keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,47 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Add any additional keymaps here
+-- Pindah code alt + up/down
+-- vim.keymap.set("i", "<A-Up>", "<Esc>yyPgi", { noremap = true, silent = true })
+-- vim.keymap.set("i", "<A-Down>", "<Esc>yypgi", { noremap = true, silent = true })
 
--- 1. Copy/Paste (Insert/Normal)
-vim.keymap.set({ "n", "i", "v" }, "<C-c>", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set({ "n", "i", "v" }, "<C-v>", '"+p', { desc = "Paste from clipboard" })
-
--- 2. Duplicate Line (Insert/Normal)
-vim.keymap.set("n", "<S-A-Up>", "yyP", { desc = "Duplicate line up" })
-vim.keymap.set("n", "<S-A-Down>", "yyp", { desc = "Duplicate line down" })
-vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { desc = "Duplicate line up" })
-vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { desc = "Duplicate line down" })
-
--- 3. Move Line (Normal/Insert/Visual)
-vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-vim.keymap.set("i", "<A-Up>", "<Esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
-vim.keymap.set("i", "<A-Down>", "<Esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
-vim.keymap.set("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
-vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
-
--- 4. Delete Line
-vim.keymap.set({ "n", "i" }, "<C-S-k>", "<cmd>normal! dd<cr>", { desc = "Delete line" })
-
--- 5. Toggle Terminal (Normal mode only)
-vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
-
--- 6. Text Selection (Shift + Arrows)
--- Map to Visual mode explicitly for all directions
-vim.keymap.set({ "n", "i", "v" }, "<S-Left>", "<ESC>v<Left>", { desc = "Select left" })
-vim.keymap.set({ "n", "i", "v" }, "<S-Right>", "<ESC>v<Right>", { desc = "Select right" })
-vim.keymap.set({ "n", "i", "v" }, "<S-Up>", "<ESC>v<Up>", { desc = "Select up" })
-vim.keymap.set({ "n", "i", "v" }, "<S-Down>", "<ESC>v<Down>", { desc = "Select down" })
-
--- 7. Select All
--- Ensure it forces Visual mode
-vim.keymap.set({ "n", "i", "v" }, "<C-a>", "<ESC>ggVG", { desc = "Select all" })
-
--- Extras: Save/Close/Comment/Pane Nav
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-vim.keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
-vim.keymap.set({ "n", "v" }, "<C-/>", "gcc", { remap = true, desc = "Toggle Comment" })
-vim.keymap.set("i", "<C-/>", "<Esc>gccgi", { remap = true, desc = "Toggle Comment" })
-vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to Left Pane" })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to Down Pane" })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to Up Pane" })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Go to Right Pane" })
+-- Undo dengan Ctrl+Z di mode Insert
+vim.keymap.set("i", "<C-z>", "<Esc>ui", { noremap = true, silent = true })
+-- Redo dengan Ctrl+Y di mode Insert
+vim.keymap.set("i", "<C-y>", "<Esc><C-r>i", { noremap = true, silent = true })
+-- Hapus seluruh baris saat ini di mode Insert (Ctrl+Backspace)
+vim.keymap.set("i", "<C-Backspace>", "<Esc>ddi", { noremap = true, silent = true })
+-- Select All di mode Insert (Ctrl+A)
+vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+-- Duplicate line di mode Insert Shift + Alt + Up/Down
+vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { noremap = true, silent = true })
+vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { noremap = true, silent = true })
+-- Copy (Ctrl + C) dan Paste (Ctrl + V) di mode Insert
+vim.keymap.set("i", "<C-c>", "<Esc>yyi", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-v>", "<Esc>pgi", { noremap = true, silent = true })
