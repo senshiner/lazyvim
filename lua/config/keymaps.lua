d4e007d17882bb8155ba0fede6f9da6ebb7001ef
Sendi Winoto <sendiw.cep@gmail.com>
2026-07-15 17:48:09 +0700

    feat: implement VS Code-like keybindings via mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Add any additional keymaps here
--- Pindah code alt + up/down
--- vim.keymap.set("i", "<A-Up>", "<Esc>yyPgi", { noremap = true, silent = true })
--- vim.keymap.set("i", "<A-Down>", "<Esc>yypgi", { noremap = true, silent = true })
 
--- Undo dengan Ctrl+Z di mode Insert
-vim.keymap.set("i", "<C-z>", "<Esc>ui", { noremap = true, silent = true })
--- Redo dengan Ctrl+Y di mode Insert
-vim.keymap.set("i", "<C-y>", "<Esc><C-r>i", { noremap = true, silent = true })
--- Hapus seluruh baris saat ini di mode Insert (Ctrl+Backspace)
-vim.keymap.set("i", "<C-Backspace>", "<Esc>ddi", { noremap = true, silent = true })
--- Select All di mode Insert (Ctrl+A)
-vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
--- Duplicate line di mode Insert Shift + Alt + Up/Down
-vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { noremap = true, silent = true })
-vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { noremap = true, silent = true })
--- Copy (Ctrl + C) dan Paste (Ctrl + V) di mode Insert
-vim.keymap.set("i", "<C-c>", "<Esc>yyi", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-v>", "<Esc>pgi", { noremap = true, silent = true })
+-- 1. Essential VS Code-like Keybindings (Mapped)
+
+-- Copy/Paste in Insert Mode
+vim.keymap.set("i", "<C-c>", "<Esc>yi", { desc = "Copy to clipboard" })
+vim.keymap.set("i", "<C-v>", "<Esc>pi", { desc = "Paste from clipboard" })
+
+-- Duplicate Line (Normal mode)
+vim.keymap.set("n", "<S-A-Up>", "yyP", { desc = "Duplicate line up" })
+vim.keymap.set("n", "<S-A-Down>", "yyp", { desc = "Duplicate line down" })
+
+-- Move Line (Normal mode)
+vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+
+-- Select All (Normal mode)
+vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
+
+-- Delete Line (Normal mode)
+vim.keymap.set("n", "<C-S-k>", "dd", { desc = "Delete line" })
+
+-- Toggle Terminal (Normal mode)
+vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+
+-- File Tree Explorer (Default LazyVim)
+-- SPC e is default, keeping it for clarity in shortcuts
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle File Tree" })
