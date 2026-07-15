70fd6253473a48780f1778eb527847606000a13f
Sendi Winoto <sendiw.cep@gmail.com>
2026-07-15 17:03:29 +0700

    feat: apply VS Code-like keymaps with conflict resolution

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,14 +7,14 @@ vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save fil
 -- Close buffer with Ctrl + w
 vim.keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
 
--- Toggle Terminal with Ctrl + `
-vim.keymap.set({ "n", "t" }, "<C-`>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+-- Toggle Terminal with Ctrl + t (Safer alternative to <C-`>)
+vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
 
 -- Commenting with Ctrl + /
 vim.keymap.set({ "n", "v" }, "<C-/>", "gcc", { remap = true, desc = "Toggle Comment" })
 vim.keymap.set("i", "<C-/>", "<Esc>gccgi", { remap = true, desc = "Toggle Comment" })
 
--- Pane Navigation
+-- Pane Navigation using Alt
 vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to Left Pane" })
 vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to Down Pane" })
 vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to Up Pane" })
@@ -28,18 +28,13 @@ vim.keymap.set("i", "<A-Down>", "<Esc><cmd>m .+1<cr>==gi", { desc = "Move line d
 vim.keymap.set("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
 vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
 
--- Undo dengan Ctrl+Z di mode Insert
+-- Existing custom keymaps
 vim.keymap.set("i", "<C-z>", "<Esc>ui", { noremap = true, silent = true })
--- Redo dengan Ctrl+Y di mode Insert
 vim.keymap.set("i", "<C-y>", "<Esc><C-r>i", { noremap = true, silent = true })
--- Hapus seluruh baris saat ini di mode Insert (Ctrl+Backspace)
 vim.keymap.set("i", "<C-Backspace>", "<Esc>ddi", { noremap = true, silent = true })
--- Select All di mode Insert (Ctrl+A)
 vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
--- Duplicate line di mode Insert Shift + Alt + Up/Down
 vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { noremap = true, silent = true })
 vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { noremap = true, silent = true })
--- Copy (Ctrl + C) dan Paste (Ctrl + V) di mode Insert
 vim.keymap.set("i", "<C-c>", "<Esc>yyi", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-v>", "<Esc>pgi", { noremap = true, silent = true })
