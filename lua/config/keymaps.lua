3f5856b0d2b7121d707829483f060789329defa7
Sendi Winoto <sendiw.cep@gmail.com>
2026-07-15 17:08:23 +0700

    feat: refine keymaps to match VS Code essential shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,26 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Add any additional keymaps here
 
--- Save with Ctrl + s
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+-- 1. Copy/Paste (Insert/Normal)
+vim.keymap.set({ "n", "i", "v" }, "<C-c>", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set({ "n", "i", "v" }, "<C-v>", '"+p', { desc = "Paste from clipboard" })
 
--- Close buffer with Ctrl + w
-vim.keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
+-- 2. Duplicate Line (Insert/Normal)
+vim.keymap.set("n", "<S-A-Up>", "yyP", { desc = "Duplicate line up" })
+vim.keymap.set("n", "<S-A-Down>", "yyp", { desc = "Duplicate line down" })
+vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { desc = "Duplicate line up" })
+vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { desc = "Duplicate line down" })
 
--- Toggle Terminal with Ctrl + t (Safer alternative to <C-`>)
-vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
-
--- Commenting with Ctrl + /
-vim.keymap.set({ "n", "v" }, "<C-/>", "gcc", { remap = true, desc = "Toggle Comment" })
-vim.keymap.set("i", "<C-/>", "<Esc>gccgi", { remap = true, desc = "Toggle Comment" })
-
--- Pane Navigation using Alt
-vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to Left Pane" })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to Down Pane" })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to Up Pane" })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Go to Right Pane" })
-
--- Move line up/down with Alt + Up/Down
+-- 3. Move Line (Normal/Insert/Visual)
 vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
 vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
 vim.keymap.set("i", "<A-Up>", "<Esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
@@ -28,13 +19,18 @@ vim.keymap.set("i", "<A-Down>", "<Esc><cmd>m .+1<cr>==gi", { desc = "Move line d
 vim.keymap.set("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
 vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
 
--- Existing custom keymaps
-vim.keymap.set("i", "<C-z>", "<Esc>ui", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-y>", "<Esc><C-r>i", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-Backspace>", "<Esc>ddi", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
-vim.keymap.set("i", "<S-A-Up>", "<Esc>yyPi", { noremap = true, silent = true })
-vim.keymap.set("i", "<S-A-Down>", "<Esc>yypi", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-c>", "<Esc>yyi", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-v>", "<Esc>pgi", { noremap = true, silent = true })
+-- 4. Delete Line
+vim.keymap.set({ "n", "i" }, "<C-S-k>", "<cmd>normal! dd<cr>", { desc = "Delete line" })
+
+-- 5. Toggle Terminal
+vim.keymap.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+
+-- Extras: Save/Close/Comment/Pane Nav
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
+vim.keymap.set({ "n", "v" }, "<C-/>", "gcc", { remap = true, desc = "Toggle Comment" })
+vim.keymap.set("i", "<C-/>", "<Esc>gccgi", { remap = true, desc = "Toggle Comment" })
+vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to Left Pane" })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to Down Pane" })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to Up Pane" })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Go to Right Pane" })
